89112552c6c997b000938756bdae148eba11e603
hibana2077 <hibana2077@gmail.com>
2023-02-16 13:33:52 +0800

    01

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2023-02-16 13:21:40
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-02-16 13:27:37
+ * @FilePath: \vue_test\test01\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your first Vue.js App"/>
 </template>
 
 <script>
